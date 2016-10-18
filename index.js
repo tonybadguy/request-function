e20b736fb020c2753fa1b54d1cbd15202a881ab8
Tony Liu <tony@zutanolabs.com>
2016-10-18 00:37:53 -0700

    initial code commit

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+const http = require('http');
+const https = require('https');
+const url = require('url');
+const typedError = require('error/typed');
+
+let unknownProtocolError = typedError({
+  type: 'request-function/unknown-protocol',
+  message: "Unknown protocol '{protocol}'"
+});
+
+module.exports = (input) => {
+  let parsedUrl = url.parse(input);
+
+  switch(parsedUrl.protocol){
+    case 'http:':
+      return http.request;
+    case 'https:':
+      return https.request;
+    default:
+      throw unknownProtocolError({
+        protocol: parsedUrl.protocol
+      });
+  }
+}
---
+++ index.test.js
@@ -0,0 +1,33 @@
+'use strict';
+const getRequestFunction = require('./index')
+const test = require('tape-catch');
+
+test('http protocol returns correct request', (assert => {
+  assert.plan(1);
+  
+  var requestFunction = getRequestFunction('http://localhost');
+  assert.equal(requestFunction, require('http').request);
+
+}));
+
+test('https protocol returns correct request', (assert => {
+  assert.plan(1);
+  
+  var requestFunction = getRequestFunction('https://localhost');
+  assert.equal(requestFunction, require('https').request);
+
+}));
+
+test('unknown protocol throws correct error', (assert) => {
+  assert.plan(2);
+  
+  const getRequestFunction = require('./index')
+
+  try{
+    getRequestFunction('telnet://localhost');
+  }
+  catch(e){
+    assert.equal(e.type, 'request-function/unknown-protocol');
+    assert.equal(e.message, "Unknown protocol 'telnet:'");
+  }
+});
