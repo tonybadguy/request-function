d4c9a7feb94df04ae9ca7e64aada16b856658cf8
Tony Liu <tony@zutanolabs.com>
2016-10-22 18:31:47 -0700

    unit tests

--- index.test.js
+++ index.test.js
@@ -2,7 +2,7 @@
 const requestFunction = require('./index');
 const test = require('tap').test;
 
-test('http protocol returns correct request', (assert => {
+test('http url returns correct request', (assert => {
   assert.plan(1);
   
   var request = requestFunction.fromUrl('http://localhost');
@@ -10,10 +10,40 @@ test('http protocol returns correct request', (assert => {
 
 }));
 
-test('https protocol returns correct request', (assert => {
+test('https url returns correct request', (assert => {
   assert.plan(1);
   
   var request = requestFunction.fromUrl('https://localhost');
+  assert.equal(request, require('https').request);
+
+}));
+
+test('unknown url throws correct error', (assert) => {
+  assert.plan(2);
+  
+  const requestFunction = require('./index');
+
+  try{
+    requestFunction.fromUrl('telnet://localhost');
+  }
+  catch(e){
+    assert.equal(e.type, requestFunction.unknownProtocolErrorType);
+    assert.equal(e.message, "Unknown protocol 'telnet:'");
+  }
+});
+
+test('http protocol returns correct request', (assert => {
+  assert.plan(1);
+  
+  var request = requestFunction.fromProtocol('http:');
+  assert.equal(request, require('http').request);
+
+}));
+
+test('https protocol returns correct request', (assert => {
+  assert.plan(1);
+  
+  var request = requestFunction.fromProtocol('https:');
   assert.equal(request, require('https').request);
 
 }));
@@ -24,10 +54,16 @@ test('unknown protocol throws correct error', (assert) => {
   const requestFunction = require('./index');
 
   try{
-    requestFunction.fromUrl('telnet://localhost');
+    requestFunction.fromProtocol('telnet:');
   }
   catch(e){
-    assert.equal(e.type, 'request-function/unknown-protocol');
+    assert.equal(e.type, requestFunction.unknownProtocolErrorType);
     assert.equal(e.message, "Unknown protocol 'telnet:'");
   }
+});
+
+test('unknownProtocolErrorType is correct', (assert) => {
+  assert.plan(1);
+  
+  assert.equal('request-function/unknown-protocol', requestFunction.unknownProtocolErrorType);
 });
